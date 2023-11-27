9d31f2fda8445d1d87191c6533e17a142cd312d2
cluttrdev <andreas@cluttr.tech>
2023-11-27 21:50:17 +0100

    style: Format main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "errors"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -10,39 +10,39 @@ import (
 )
 
 func main() {
-    if err := execute(); err != nil {
-        fmt.Fprintln(os.Stderr, err.Error())
-        os.Exit(1)
-    }
+	if err := execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
 
 func execute() error {
-    auth_key := parseAuthKey()
-    if auth_key == "" {
-        return errors.New("Missing required authentication key.")
-    }
+	auth_key := parseAuthKey()
+	if auth_key == "" {
+		return errors.New("Missing required authentication key.")
+	}
 
 	translator, err := deepl.NewTranslator(auth_key)
 	if err != nil {
-        return err
+		return err
 	}
 
 	app, err := NewApplication(translator)
 	if err != nil {
-        return err
+		return err
 	}
 
 	return app.Run()
 }
 
 func parseAuthKey() string {
-    // parse args
-    f := flag.String("auth-key", "", "the authentication key as given in your DeepL account.")
-    flag.Parse()
-    if *f != "" {
-        return *f
-    }
+	// parse args
+	f := flag.String("auth-key", "", "the authentication key as given in your DeepL account.")
+	flag.Parse()
+	if *f != "" {
+		return *f
+	}
 
-    // parse env
-    return os.Getenv("DEEPL_AUTH_KEY")
+	// parse env
+	return os.Getenv("DEEPL_AUTH_KEY")
 }
